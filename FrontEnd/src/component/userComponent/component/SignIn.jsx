70e9e5d5070111afefdb0ab5c3e93ac56d518939
Dinusha-hue <dinushaweerasekara312@gmail.com>
2023-04-18 12:56:01 +0530

    addcssforfrontend

--- FrontEnd/src/component/userComponent/component/SignIn.jsx
+++ FrontEnd/src/component/userComponent/component/SignIn.jsx
@@ -1,125 +1,139 @@
-import { useFormik } from "formik";
-import { Box, Typography, Button } from "@mui/material";
-import TextField from "@mui/material/TextField";
-import { makeStyles } from "tss-react/mui";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import * as Yup from "yup";
 import axios from "axios";
-// import Swal from 'sweetalert2'
-import { Link } from 'react-router-dom'
-import avatar from '../assets/profile.png'
-import Avatar from "@mui/material/Avatar";
+import avatar from '../assets/profile.png';
+import { useFormik } from "formik";
+import { makeStyles } from "tss-react/mui";
 
-//import { Toaster } from 'react-hot-toast';
-// import { apartmentNoValidate } from "../helper/validate";
-
-
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const validationSchema = Yup.object({
   
-  apartmentNo: Yup.string()
-    .length(3)
-    .matches( 
-      /^[a-zA-Z]\d{2}$/,
-      "use one letter and two number format \nex: 'A10' "
-    )
-    .required("apartment number is required"),
-  
-});
-
-const useStyles = makeStyles()((theme) => ({
-  root: {
-    [theme.breakpoints.up("md")]: {
-      width: "30%",
-    },
-    [theme.breakpoints.down("md")]: {
-      width: "60%",
-    },
-    [theme.breakpoints.down("sm")]: {
-      width: "95%",
-    },
-    margin: "0 auto",
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    marginTop: "30px",
-    marginRight: "30%",
-    justifyContent: "center",
-  },
-  formControl: {
-    marginTop: "10px",
-  },
-  submitBtn: {
-    marginTop: "75px",
-    marginLeft: "16%",
-  },
-  signin: {
-    marginRight: "70%",
-  },
-  link1:{
-    marginLeft: "-100px",
-    color: "#000000",
-    "&:hover": {
-        color: "#FF0000",
-        textDecoration: "underline #000000"
-    }
-  },
-  link2:{
-    marginLeft: "60px",
-    color: "#000000",
-    "&:hover": {
-        color: "#FF0000",
-        textDecoration: "underline #000000"
-    }
-  },
-
-  
-}));
-
-export default function SignIn() {
-  const { classes } = useStyles();
-
-  const formik = useFormik({
-    initialValues: {
-      
-      apartmentNo: "",
-      password:"",
-      
-    },
-    validationSchema: validationSchema,
-    validateOnChange: true,
-    onSubmit: async values =>{
-      axios({ method: "POST", url: "http://localhost:5000/customer/login", data: { apartmentNo: values.apartmentNo , password: values.password } });
-    }
+    apartmentNo: Yup.string()
+      .length(3)
+      .matches( 
+        /^[a-zA-Z]\d{2}$/,
+        "use one letter and two number format \nex: 'A10' "
+      )
+      .required("apartment number is required"),
+    
   });
 
-  const { handleChange, handleSubmit } = formik;
+  const useStyles = makeStyles()((theme) => ({
+    root: {
+      [theme.breakpoints.up("md")]: {
+        width: "30%",
+      },
+      [theme.breakpoints.down("md")]: {
+        width: "60%",
+      },
+      [theme.breakpoints.down("sm")]: {
+        width: "95%",
+      },
+      margin: "0 auto",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      marginTop: "30px",
+      marginRight: "30%",
+      justifyContent: "center",
+    },
+    formControl: {
+      marginTop: "10px",
+    },
+    submitBtn: {
+      marginTop: "75px",
+      marginLeft: "16%",
+    },
+    signin: {
+      marginRight: "70%",
+    },
+    link1:{
+      marginLeft: "-100px",
+      color: "#000000",
+      "&:hover": {
+          color: "#FF0000",
+          textDecoration: "underline #000000"
+      }
+    },
+    link2:{
+      marginLeft: "60px",
+      color: "#000000",
+      "&:hover": {
+          color: "#FF0000",
+          textDecoration: "underline #000000"
+      }
+    },
+  
+    
+  }));
 
-    return (
-    <Box
-      component='form'
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-        justifyContent:'center',
-      }}
-      noValidate
-      autoComplete='off'
-      className={classes.root}
-      onSubmit={handleSubmit}>
-      <Typography variant='h4' className={classes.signin}  align='center'>
-        Sign-In
-      </Typography>
+const theme = createTheme();
 
-      <div className="flex justoify-center items-center h-screen">
+export default function SignIn() {
+    const { classes } = useStyles();
 
-        <div >
-          <Avatar
-              alt="Remy Sharp"
-              align="center"
-              src={avatar}
-              sx={{ width: 210, height: 200, marginRight:"10%" }}
-          />
-          
+    const formik = useFormik({
+      initialValues: {
+        
+        apartmentNo: "",
+        password:"",
+        
+      },
+      validationSchema: validationSchema,
+      validateOnChange: true,
+      onSubmit: async values =>{
+        axios({ method: "POST", url: "http://localhost:8000/customer/login", data: { apartmentNo: values.apartmentNo , password: values.password } });
+       // console.log("success")
+      }
+    });
+
+    const { handleChange, handleSubmit } = formik;
+ 
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar alt="Remy Sharp" src={avatar}/>
+                   
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
+              margin="normal"
+              fullWidth
               id='apartmentNo'
               name='apartmentNo'
               label='Apartment No'
@@ -139,8 +153,9 @@ export default function SignIn() {
                   : null
               }
             />
-            
             <TextField
+              margin="normal"
+              fullWidth
               id='password'
               name='password'
               label='Password'
@@ -158,39 +173,35 @@ export default function SignIn() {
                   : null
               }
             />
-            
-            <br />
-            <Button type='submit' className={classes.submitBtn} variant='contained'>
-              Loging
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
             </Button>
 
-            <Link
-              component="button"
-              className={classes.link1}
-              variant="body2"
-              onClick={() => {
-              
-              }}
-              to="/recovery"
-            >
-              Fortet Password
-            </Link>
-
-            <br/>
-
-            <Link
-              component="button"
-              className={classes.link2}
-              variant="body2"
-              underline="none"
-              
-              to="/add"
-            >
-              Not a Member
-            </Link>
-            
-          </div>
-        </div>
-    </Box>
+          </Box>
+          <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link variant="body2" component="button" to="/add">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
 }
--- FrontEnd/src/component/userComponent/component/SignUp.jsx
+++ FrontEnd/src/component/userComponent/component/SignUp.jsx
@@ -1,252 +1,324 @@
-import { useFormik } from "formik";
-import { Box, Typography, Button } from "@mui/material";
-import TextField from "@mui/material/TextField";
-import { makeStyles } from "tss-react/mui";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import * as Yup from "yup";
 import axios from "axios";
 import Swal from 'sweetalert2'
+import { useFormik } from "formik";
+import { makeStyles } from "tss-react/mui";
 
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 const validationSchema = Yup.object({
-  name: Yup.string().min(2).required("name is required"),
-  apartmentNo: Yup.string()
-    .length(3)
-    .matches(
-      /^[a-zA-Z]\d{2}$/,
-      "use one letter and two number format \nex: 'A10' "
-    )
-    .required("apartment number is required"),
-  phoneNo: Yup.number().required("phone number is required"),
-  nicNo: Yup.string().required("nic is required"),
-  password: Yup.string().min(5).required("Password is required"),
-  confPassword: Yup.string().oneOf(
-    [Yup.ref("password"), null],
-    "Passwords must match"
-  ),
-  email: Yup.string().email("Invalid email address").required("Required"),
-});
-
-const useStyles = makeStyles()((theme) => ({
-  root: {
-    [theme.breakpoints.up("md")]: {
-      width: "30%",
-    },
-    [theme.breakpoints.down("md")]: {
-      width: "60%",
-    },
-    [theme.breakpoints.down("sm")]: {
-      width: "95%",
-    },
-    margin: "0 auto",
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    marginTop: "30px",
-    marginRight: "30%",
-  },
-  formControl: {
-    marginTop: "10px",
-  },
-  submitBtn: {
-    marginTop: "75px",
-    marginLeft: "15%",
-  },
-  signup: {
-    marginRight: "80%",
-  },
-}));
-
-export default function SignUp() {
-  const { classes } = useStyles();
-
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      apartmentNo: "",
-      email: "",
-      phoneNo: "",
-      nicNo: "",
-      confPassword: "",
-      password: "",
-    },
-    validationSchema: validationSchema,
-    validateOnChange: true,
-    onSubmit: (values) => {
-      if (values.confPassword === values.password) {
-        
-        axios({ method: "POST", url: "http://localhost:5000/sendOTP/otp", data: { email: values.email } });
-        Swal.fire({
-          title: "Enter OTP",
-          input: "text",
-          inputLabel: "",
-          inputPlaceholder: "Enter otp",
-          showCancelButton: true,
-          confirmButtonText: "Submit",
-          preConfirm: (otp) => {
-            return axios
-              .post("http://localhost:5000/sendOTP/verifyOTP", { values,otp })
-              .then((response) => {
-                return response.data;
-              })
-              .catch((error) => {
-                Swal.showValidationMessage(`Request failed: ${error}`);
-              });
-          },
-        }).then((result) => {
-          if (result.value) {
-            Swal.fire("Submitted!", '', "success");
-            axios({ method: "POST", url: "http://localhost:5000/customer/add", data: { name: values.name,appartmentNo: values.apartmentNo,email: values.email,phoneNo: values.phoneNo,nicNo: values.nicNo,password: values.password } }).then(()=>{
-              alert("Customer added")
-            }).catch((err)=>{
-              alert(err)
-            })
-          }
-        })
-        .catch((err)=>{
-          alert(err)
-        });
-      }
-    },
+    name: Yup.string().min(2).required("name is required"),
+    apartmentNo: Yup.string()
+      .length(3)
+      .matches(
+        /^[a-zA-Z]\d{2}$/,
+        "use one letter and two number format \nex: 'A10' "
+      )
+      .required("apartment number is required"),
+    phoneNo: Yup.number().required("phone number is required"),
+    nicNo: Yup.string().required("nic is required"),
+    password: Yup.string().min(5).required("Password is required"),
+    confPassword: Yup.string().oneOf(
+      [Yup.ref("password"), null],
+      "Passwords must match"
+    ),
+    email: Yup.string().email("Invalid email address").required("Required"),
   });
 
-  const { handleChange, handleSubmit } = formik;
+  const useStyles = makeStyles()((theme) => ({
+    root: {
+      [theme.breakpoints.up("md")]: {
+        width: "30%",
+      },
+      [theme.breakpoints.down("md")]: {
+        width: "60%",
+      },
+      [theme.breakpoints.down("sm")]: {
+        width: "95%",
+      },
+      margin: "0 auto",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      marginTop: "30px",
+      marginRight: "30%",
+    },
+    formControl: {
+      marginTop: "10px",
+    },
+    submitBtn: {
+      marginTop: "75px",
+      marginLeft: "15%",
+    },
+    signup: {
+      marginRight: "80%",
+    },
+  }));
 
-    return (
-    <Box
-      component='form'
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete='off'
-      className={classes.root}
-      onSubmit={handleSubmit}>
-      <Typography variant='h4' className={classes.signup}>
-        Sign-Up
-      </Typography>
+export default function SignUp() {
 
-      <div>
-        <TextField
-          id='name'
-          name='name'
-          label='Name'
-          type='text'
-          value={formik.values.name}
-          className={classes.name}
-          placeholder='Enter name'
-          onChange={handleChange}
-          error={formik.errors["name"] && formik.touched.name ? true : false}
-          helperText={
-            formik.errors["name"] && formik.touched.name
-              ? formik.errors["name"]
-              : null
+    const { classes } = useStyles();
+
+    const formik = useFormik({
+        initialValues: {
+          name: "",
+          apartmentNo: "",
+          email: "",
+          phoneNo: "",
+          nicNo: "",
+          confPassword: "",
+          password: "",
+        },
+        validationSchema: validationSchema,
+        validateOnChange: true,
+        onSubmit: (values) => {
+          if (values.confPassword === values.password) {
+            
+            axios({ method: "POST", url: "http://localhost:8000/sendOTP/otp", data: { email: values.email } });
+            Swal.fire({
+              title: "Enter OTP",
+              input: "text",
+              inputLabel: "",
+              inputPlaceholder: "Enter otp",
+              showCancelButton: true,
+              confirmButtonText: "Submit",
+              preConfirm: (otp) => {
+                return axios
+                  .post("http://localhost:8000/sendOTP/verifyOTP", { values,otp })
+                  .then((response) => {
+                    return response.data;
+                  })
+                  .catch((error) => {
+                    Swal.showValidationMessage(`Request failed: ${error}`);
+                  });
+              },
+            }).then((result) => {
+              if (result.value) {
+                Swal.fire("Submitted!", '', "success");
+                axios({ method: "POST", url: "http://localhost:8000/customer/add", data: { name: values.name,appartmentNo: values.apartmentNo,email: values.email,phoneNo: values.phoneNo,nicNo: values.nicNo,password: values.password } }).then(()=>{
+                  alert("Customer added")
+                }).catch((err)=>{
+                  alert(err)
+                })
+              }
+            })
+            .catch((err)=>{
+              alert(err)
+            });
           }
-        />
-        <TextField
-          id='apartmentNo'
-          name='apartmentNo'
-          label='Apartment No'
-          type='text'
-          value={formik.values.apartmentNo}
-          className={classes.apartmentNo}
-          placeholder='Enter apartment No'
-          error={
-            formik.errors["apartmentNo"] && formik.touched.apartmentNo
-              ? true
-              : false
-          }
-          onChange={handleChange}
-          helperText={
-            formik.errors["apartmentNo"] && formik.touched.apartmentNo
-              ? formik.errors["apartmentNo"]
-              : null
-          }
-        />
-        <TextField
-          id='nicNo'
-          name='nicNo'
-          label='NIC No'
-          type='text'
-          value={formik.values.nicNo}
-          className={classes.nicNo}
-          placeholder='Enter NIC No'
-          error={formik.errors["nicNo"] && formik.touched.nicNo ? true : false}
-          onChange={handleChange}
-          helperText={
-            formik.errors["nicNo"] && formik.touched.nicNo
-              ? formik.errors["nicNo"]
-              : null
-          }
-        />
-        <TextField
-          id='phoneNo'
-          name='phoneNo'
-          label='Phone No'
-          type='text'
-          className={classes.phoneNo}
-          value={formik.values.phoneNo}
-          placeholder='Enter phone number'
-          error={
-            formik.errors["phoneNo"] && formik.touched.phoneNo ? true : false
-          }
-          onChange={handleChange}
-          helperText={
-            formik.errors["phoneNo"] && formik.touched.phoneNo
-              ? formik.errors["phoneNo"]
-              : null
-          }
-        />
-        <TextField
-          id='email'
-          name='email'
-          label='Email'
-          type='email'
-          className={classes.email}
-          value={formik.values.email}
-          error={formik.errors["email"] && formik.touched.email ? true : false}
-          placeholder='Enter email'
-          onChange={handleChange}
-          helperText={
-            formik.errors["email"] && formik.touched.email
-              ? formik.errors["email"]
-              : null
-          }
-        />
-        <TextField
-          id='password'
-          name='password'
-          label='Password'
-          type='password'
-          className={classes.password}
-          value={formik.values.password}
-          error={
-            formik.errors["password"] && formik.touched.password ? true : false
-          }
-          placeholder='Enter password'
-          onChange={handleChange}
-          helperText={
-            formik.errors["password"] && formik.touched.password
-              ? formik.errors["password"]
-              : null
-          }
-        />
-        <TextField
-          name='confPassword'
-          label='Conform Password'
-          type='password'
-          autoComplete='current-password'
-          value={formik.values.confPassword}
-          error={formik.errors["confPassword"] ? true : false}
-          onChange={handleChange}
-          helperText={
-            formik.errors["confPassword"] ? formik.errors["confPassword"] : null
-          }
-        />
-        <br />
-        <Button type='submit' className={classes.submitBtn} variant='contained'>
-          Get OTP
-        </Button>
-      </div>
-    </Box>
+        },
+      });
+
+      const { handleChange, handleSubmit } = formik;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+          
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id='name'
+                  name='name'
+                  label='Name'
+                  type='text'
+                  value={formik.values.name}
+                  className={classes.name}
+                  placeholder='Enter name'
+                  onChange={handleChange}
+                  error={formik.errors["name"] && formik.touched.name ? true : false}
+                  helperText={
+                    formik.errors["name"] && formik.touched.name
+                      ? formik.errors["name"]
+                      : null
+                  }
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id='apartmentNo'
+                  name='apartmentNo'
+                  label='Apartment No'
+                  type='text'
+                  value={formik.values.apartmentNo}
+                  className={classes.apartmentNo}
+                  placeholder='Enter apartment No'
+                  error={
+                    formik.errors["apartmentNo"] && formik.touched.apartmentNo
+                      ? true
+                      : false
+                  }
+                  onChange={handleChange}
+                  helperText={
+                    formik.errors["apartmentNo"] && formik.touched.apartmentNo
+                      ? formik.errors["apartmentNo"]
+                      : null
+                  }
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id='nicNo'
+                  name='nicNo'
+                  label='NIC No'
+                  type='text'
+                  value={formik.values.nicNo}
+                  className={classes.nicNo}
+                  placeholder='Enter NIC No'
+                  error={formik.errors["nicNo"] && formik.touched.nicNo ? true : false}
+                  onChange={handleChange}
+                  helperText={
+                    formik.errors["nicNo"] && formik.touched.nicNo
+                      ? formik.errors["nicNo"]
+                      : null
+                  }
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id='phoneNo'
+                  name='phoneNo'
+                  label='Phone No'
+                  type='text'
+                  className={classes.phoneNo}
+                  value={formik.values.phoneNo}
+                  placeholder='Enter phone number'
+                  error={
+                    formik.errors["phoneNo"] && formik.touched.phoneNo ? true : false
+                  }
+                  onChange={handleChange}
+                  helperText={
+                    formik.errors["phoneNo"] && formik.touched.phoneNo
+                      ? formik.errors["phoneNo"]
+                      : null
+                  }
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id='email'
+                  name='email'
+                  label='Email'
+                  type='email'
+                  className={classes.email}
+                  value={formik.values.email}
+                  error={formik.errors["email"] && formik.touched.email ? true : false}
+                  placeholder='Enter email'
+                  onChange={handleChange}
+                  helperText={
+                    formik.errors["email"] && formik.touched.email
+                      ? formik.errors["email"]
+                      : null
+                  }
+                />
+              </Grid>
+              <Grid item xs={12} >
+                <TextField
+                  fullWidth
+                  id='password'
+                  name='password'
+                  label='Password'
+                  type='password'
+                  className={classes.password}
+                  value={formik.values.password}
+                  error={
+                    formik.errors["password"] && formik.touched.password ? true : false
+                  }
+                  placeholder='Enter password'
+                  onChange={handleChange}
+                  helperText={
+                    formik.errors["password"] && formik.touched.password
+                      ? formik.errors["password"]
+                      : null
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                 fullWidth
+                 name='confPassword'
+                 label='Conform Password'
+                 type='password'
+                 autoComplete='current-password'
+                 value={formik.values.confPassword}
+                 error={formik.errors["confPassword"] ? true : false}
+                 onChange={handleChange}
+                 helperText={
+                   formik.errors["confPassword"] ? formik.errors["confPassword"] : null
+                 }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Get OTP
+            </Button>
+
+          </Box>
+          <br></br>
+          <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link component="button"  variant="body2" to="/login">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
   );
 }
