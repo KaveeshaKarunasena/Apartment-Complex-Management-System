85b8365693d1d72f0ede8eb6e1655e31ef3f9b7b
IT21200410 <it21200410@my.sliit.lk>
2023-04-28 16:43:39 +0530

    Search functionality complete for two inputs

--- FrontEnd/src/component/managerComponents/serviceProvider.jsx
+++ FrontEnd/src/component/managerComponents/serviceProvider.jsx
@@ -57,6 +57,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 const ServiceProvider = () => {
   const [showForm, setShowForm] = React.useState(false);
 
+  const [defaultServiceProviders, setdefaultServiceProviders] = useState([]);
   const [serviceProviders, setServiceProviders] = useState([]);
   const [isService, setIsService] = useState(false);
 
@@ -72,13 +73,8 @@ const ServiceProvider = () => {
     
     const searchValue = searchedVal.target.value;
 
-    if (searchValue == "")
-    {
-      setIsService(true);
-    }
-
-    const filteredRows = serviceProviders.filter((serviceProvider) => {
-      return serviceProvider.location.toLowerCase().includes(searchValue.toLowerCase());
+    const filteredRows = defaultServiceProviders.filter((serviceProvider) => {
+      return serviceProvider.location.toLowerCase().includes(searchValue.toLowerCase()) || serviceProvider.serviceType.toLowerCase().includes(searchValue.toLowerCase())
     });
     setServiceProviders(filteredRows);
   };
@@ -90,6 +86,7 @@ const ServiceProvider = () => {
       const json = await response.json();
 
       if (response.ok) {
+        setdefaultServiceProviders(json);
         setServiceProviders(json);
         setIsService(false);
       }
