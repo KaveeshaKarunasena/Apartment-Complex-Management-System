db846220d82c51fa2400c8de6a5fab3507ff94b8
Dinusha-hue <dinushaweerasekara312@gmail.com>
2023-05-17 16:27:38 +0530

    fix the error main branch

--- FrontEnd/src/component/adminComponents/maniDash/Navbar.jsx
+++ FrontEnd/src/component/adminComponents/maniDash/Navbar.jsx
@@ -7,8 +7,8 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
-
-import{ Menu,MenuItem }from '@material-ui/core';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
