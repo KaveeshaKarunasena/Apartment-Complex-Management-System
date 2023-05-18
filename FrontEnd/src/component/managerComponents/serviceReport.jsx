efa97259833e0964a2708cf8094c7a6a3853dd2c
IT21200410 <it21200410@my.sliit.lk>
2023-05-18 15:26:07 +0530

    Added pdf

--- FrontEnd/src/component/managerComponents/serviceReport.jsx
+++ FrontEnd/src/component/managerComponents/serviceReport.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import BarChart from './charts/BarChart';
 import LineChart from './charts/LineChart';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import axios from 'axios';
-import { Line } from 'react-chartjs-2';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 const ServiceReport = () => {
+
+
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [serviceData, setServiceData] = useState({
     labels: ['Red', 'Yellow', 'Blue'],
@@ -56,6 +59,21 @@ const ServiceReport = () => {
     },
   };
 
+  const saveAsPDF = () => {
+    const chartContainer = document.getElementById('chart-container');
+
+    html2canvas(chartContainer).then( (canvas) => {
+
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF();
+      const width = pdf.internal.pageSize.getWidth();
+      const height = pdf.internal.pageSize.getHeight();
+      pdf.addImage(imgData, 'PNG', 0,0,width, height);
+      pdf.save('chart.pdf');
+
+    });
+  }
+
   useEffect(() => {
     let getCommissionByCategory = async () => {
       const month = selectedDate.getMonth();
@@ -78,10 +96,7 @@ const ServiceReport = () => {
     };
 
     let getServicePayment = async () => {
-
-      let { data } = await axios.get(
-        `/service-provider/getServicePayment`
-      );
+      let { data } = await axios.get(`/service-provider/getServicePayment`);
 
       setPaymentData({
         labels: data.map(stat => stat._id.month),
@@ -94,19 +109,24 @@ const ServiceReport = () => {
       });
     };
 
-
-
     getCommissionByCategory();
     getServicePayment();
   }, [selectedDate]);
 
   return (
     <div>
-      <div style={{ display: 'flex', 'flex-direction': 'row', width: '80%', marginTop: '5%' }} >
-        <div style={{ flex: 1 }}>
+      <div
+        style={{
+          display: 'flex',
+          'flex-direction': 'row',
+          width: '80%',
+          marginTop: '5%',
+        }}
+      >
+        <div style={{ flex: 1 }} id = "chart-container">
           <BarChart chartData={serviceData} options={options} />
         </div>
-        <div style={{ flex: 1 , marginLeft: '5%', marginTop: '10%'}}>
+        <div style={{ flex: 1, marginLeft: '5%', marginTop: '10%' }}>
           <DatePicker
             dateFormat="MMMM yyyy"
             showMonthYearPicker
@@ -114,12 +134,19 @@ const ServiceReport = () => {
             onChange={date => setSelectedDate(date)}
           />
         </div>
-        </div>
-        <div style={{ display: 'flex', 'flex-direction': 'row', width: '80%', marginTop: '5%' }} >
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          'flex-direction': 'row',
+          width: '80%',
+          marginTop: '5%',
+        }}
+      >
         <div style={{ flex: 1 }}>
           <LineChart chartData={paymentData} options={options} />
         </div>
-        <div style={{ flex: 1 , marginLeft: '5%', marginTop: '10%'}}>
+        <div style={{ flex: 1, marginLeft: '5%', marginTop: '10%' }}>
           <DatePicker
             dateFormat="MMMM yyyy"
             showMonthYearPicker
@@ -127,7 +154,10 @@ const ServiceReport = () => {
             onChange={date => setSelectedDate(date)}
           />
         </div>
-        </div>
+      </div>
+      <div>
+        <button type="button" onClick={saveAsPDF}>Download</button>
+      </div>
     </div>
   );
 };
